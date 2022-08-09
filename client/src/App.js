b56e9e563fffb7b076c351d9853c10134762cdf7
Abhishek Gautam <099abhi@gmail.com>
2022-08-09 13:07:15 +0530

    Added the files of the app stack

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">Demo Fibo App</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
